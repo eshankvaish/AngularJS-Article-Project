0d592084e7a174590aa08e95e4e0b3d6f0531511
Eshank Vaish <eshank.vaish@joshtechnologygroup.com>
2020-12-15 16:12:24 +0530

    [AA-6] Profile - Fixed Confirm Password field on login page

--- app/modules/signup/signupController.js
+++ app/modules/signup/signupController.js
@@ -26,6 +26,7 @@ export default function signupController($scope, Auth) {
             value: '',
         },
     };
+    $scope.showConfirmPassword = true;
     // For heading
     $scope.heading = 'Sign Up';
 
